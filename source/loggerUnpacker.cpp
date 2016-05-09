99eaf96d1c1b7df5c6ae7fc50be96b20a9543dab
Cory R Thornsberry <cthornsb@vols.utk.edu>
2016-05-09 11:40:18 -0400

    loggerUnpacker exe now outputs to .csv file

--- source/loggerUnpacker.cpp
+++ source/loggerUnpacker.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <cmath>
 
 unsigned int timestamp;
 float temperature;
@@ -9,13 +10,21 @@ short relay1;
 short relay2;
 
 int main(int argc, char *argv[]){
-	if(argc < 2){ return 1; }
+	if(argc < 2){ 
+		std::cout << " Invalid number of arguments.\n";
+		std::cout << "  SYNTAX: " << argv[0] << " <filename>\n";
+		return 1; 
+	}
 
-	std::ifstream file(argv[1], std::ios::binary);
+	std::string ifname = std::string(argv[1]);
+	std::ifstream file(ifname.c_str(), std::ios::binary);
 	
-	if(!file){ return 1; }
+	if(!file.is_open()){ return 1; }
 	
-	std::cout << "time(ms)\tT(C)\tP(Torr)\tR1\tR2\n";
+	std::string ofname = ifname.substr(0, ifname.find_last_of('.'))+".csv";
+	std::ofstream output(ofname.c_str());
+	
+	output << "time(ms)\tT(C)\tP(Torr)\tR1\tR2\n";
 	
 	unsigned int count = 0;
 	while(true){
@@ -29,14 +38,16 @@ int main(int argc, char *argv[]){
 		// Convert the pressure voltage to an actual pressure.
 		pressure = pow(10.0, (pressure-5.0));
 		
-		std::cout << timestamp << "\t" << temperature << "\t" << pressure << "\t" << relay1 << "\t" << relay2 << std::endl;
+		output << timestamp << "," << temperature << "," << pressure << "," << relay1 << "," << relay2 << std::endl;
 		
 		count++;
 	}
 	
-	std::cout << "\n Done! Read " << count << " data entries.\n";
+	std::cout << " Done! Read " << count << " data entries.\n";
+	std::cout << "  Wrote output file '" << ofname << "\n";
 	
 	file.close();
+	output.close();
 	
 	return 0;
 }
