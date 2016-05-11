eaa7a767938bd365c9744d57ab35d3fce56c3885
Cory R Thornsberry <cthornsb@vols.utk.edu>
2016-05-11 16:46:14 -0500

    Fixed serial readout for raspberry pi

--- Makefile
+++ Makefile
@@ -10,41 +10,75 @@ CFLAGS = -Wall -O -Iinclude
 
 # Directories
 TOP_LEVEL = $(shell pwd)
-SRC_DIR = $(TOP_LEVEL)/source
+INCLUDE_DIR = $(TOP_LEVEL)/include
+SOURCE_DIR = $(TOP_LEVEL)/source
+OBJ_DIR = $(TOP_LEVEL)/obj
 THERMOCOUPLE_LIB = $(TOP_LEVEL)/Adafruit_MAX31855.tar
 THERMOCOUPLE_LIB_DIR = $(ARDUINO_LIB_DIR)/Adafruit_MAX31855
 SDFAT_LIB = $(TOP_LEVEL)/greiman_SdFat.tar
 SDFAT_LIB_DIR = $(ARDUINO_LIB_DIR)/greiman_SdFat
 
+# PI serial library.
+SERIAL_SRC = $(SOURCE_DIR)/wiringSerial.c
+SERIAL_OBJ = $(OBJ_DIR)/wiringSerial.o
+
 # Unpacker tool source.
-UNPACKER_SRC = $(SRC_DIR)/loggerUnpacker.cpp
+UNPACKER_SRC = $(SOURCE_DIR)/loggerUnpacker.cpp
 UNPACKER_EXE = $(TOP_LEVEL)/loggerUnpacker
 
-all: install
+########################################################################
 
-unpacker: $(UNPACKER_SRC)
+all: install unpacker
+
+########################################################################
+
+$(OBJ_DIR):
+#	Make the object file directory
+	@if [ ! -d $@ ]; then \
+		echo "Making directory: "$@; \
+		mkdir $@; \
+	fi
+
+########################################################################
+
+$(OBJ_DIR)/%.o: $(SOURCE_DIR)/%.c
+#	Compile C source files
+	$(COMPILER) -c $(CFLAGS) -Iinclude -I$(PIXIE_SUITE_DIR)/exec/include $< -o $@
+
+########################################################################
+
+unpacker: $(OBJ_DIR) $(SERIAL_OBJ) $(UNPACKER_SRC)
 #	Compile unpacker tool.
-	$(COMPILER) $(CFLAGS) -o $(UNPACKER_EXE) $(UNPACKER_SRC)
+	$(COMPILER) $(CFLAGS) -o $(UNPACKER_EXE) $(SERIAL_OBJ) $(UNPACKER_SRC)
+
+########################################################################
 
 install:
 #	Install the MAX31855 library from Adafruit.
-	@if [ ! -d $(THERMOCOUPLE_LIB_DIR) ]; then \
-		echo " Unpacking MAX31855 library files"; \
+#	And the SdFat library from https://github.com/greiman.
+	@if [ -d $(ARDUINO_LIB_DIR) ]; then \
 		cd $(ARDUINO_LIB_DIR); \
-		tar -xf $(THERMOCOUPLE_LIB); \
-	fi
-#	Install the SdFat library from https://github.com/greiman.
-	@if [ ! -d $(SDFAT_LIB_DIR) ]; then \
-		echo " Unpacking SdFat library files"; \
-		cd $(ARDUINO_LIB_DIR); \
-		tar -xf $(SDFAT_LIB); \
+		if [ ! -d $(THERMOCOUPLE_LIB_DIR) ]; then \
+			echo " Unpacking MAX31855 library files"; \
+			tar -xf $(THERMOCOUPLE_LIB); \
+		fi; \
+		if [ ! -d $(SDFAT_LIB_DIR) ]; then \
+			echo " Unpacking SdFat library files"; \
+			tar -xf $(SDFAT_LIB); \
+		fi; \
+	else \
+		echo " ERROR: directory "$(ARDUINO_LIB_DIR)" does not exist!"; \
 	fi
 
+########################################################################
+
 clean:
+	@echo " Removing compiled files"
+	@rm -f $(UNPACKER_EXE) $(OBJ_DIR)/*.o
+
+clean_lib:
 	@echo " Removing installed library files"
 	@rm -rf $(THERMOCOUPLE_LIB_DIR)/*
 	@rmdir $(THERMOCOUPLE_LIB_DIR)
 	@rm -rf $(SDFAT_LIB_DIR)/*
 	@rmdir $(SDFAT_LIB_DIR)
-	@echo " Removing compiled files"
-	@rm -f $(UNPACKER_EXE)
--- source/loggerUnpacker.cpp
+++ source/loggerUnpacker.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <vector>
 #include <cmath>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include <signal.h>
 #include <stdexcept>
+
+#include "wiringSerial.h"
 
 #define RPRIME 0.5004367
 
@@ -31,10 +37,47 @@ void setup_signal_handlers(){
 	}
 }
 
+int serialOpenB(const char *device, const int baud, const int max=1000){
+	struct termios options ;
+	int fd = serialOpen(device, baud);
+	
+	// Get and modify current options:
+	tcgetattr(fd, &options);
+	
+	options.c_lflag |= ICANON; // set canonical mode (line by line)
+	options.c_iflag |= IGNCR; // ignore CR on input
+	options.c_cc [VMIN] = max-1; // return if max-1 bytes received
+	options.c_cc [VTIME] = 0; // no timeout
+	
+	tcsetattr(fd, TCSANOW | TCSAFLUSH, &options);
+	
+	usleep(10000); // 10mS
+	
+	return fd ;
+}
+
+size_t serialGets(const int &fd_, char *buf_, const size_t &len_){
+	int numBytes = read(fd_, buf_, len_-1);
+	if(numBytes >= 0){ buf_[len_-1] = '\0'; }
+	return numBytes;
+}
+
+size_t serialRead(const int &fd_, char *val_, const size_t &len_){
+	return read(fd_, val_, len_);
+}
+
+void replaceChar(char *str_, const size_t &len_, const char &c1_, const char &c2_){
+	for(size_t index = 0; index < len_; index++){
+		if(str_[index] == c1_){ str_[index] = c2_; }
+	}
+}
+
 void help(char * prog_name_){
 	std::cout << "  SYNTAX: " << prog_name_ << " <filename> [options] [output]\n";
 	std::cout << "   Available options:\n";
-	std::cout << "    --print | Print unpacked data to stdout.\n";
+	std::cout << "    --print  | Print unpacked data to stdout.\n";
+	std::cout << "    --serial | Read data from a serial port.\n";
+	std::cout << "    --ascii  | Read ascii from the serial port.\n";
 }
 
 int main(int argc, char *argv[]){
@@ -44,24 +87,45 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
+	bool serial_mode = false;
+	bool ascii_mode = false;
 	bool printout = false;
 	int index = 2;
 	while(index < argc){
 		if(strcmp(argv[index], "--print") == 0){
 			printout = true;
 		}
+		if(strcmp(argv[index], "--serial") == 0){
+			serial_mode = true;
+		}
+		if(strcmp(argv[index], "--ascii") == 0){
+			ascii_mode = true;
+			serial_mode = true;
+		}
 		index++;
 	}
 
 	// Load the input file.
 	std::string ifname = std::string(argv[1]);
-	std::ifstream file(ifname.c_str(), std::ios::binary);
+	std::ifstream file;
+	int fd;
 
-	if(!file.is_open()){ 
-		std::cout << " ERROR: Failed to open input file '" << ifname << "'!\n";
-		return 1; 
+	if(!serial_mode){
+		file.open(argv[1], std::ios::binary);
+		if(!file.is_open()){ 
+			std::cout << " ERROR: Failed to open input file '" << argv[1] << "'!\n";
+			return 1; 
+		}
 	}
-	
+	else{
+		fd = serialOpenB(argv[1], 9600);
+		if(fd < 0){
+			std::cout << " ERROR: Failed to open serial port '" << argv[1] << "'!\n";
+			return 1; 
+		}
+		else{ std::cout << " Connected to " << argv[1] << " (fd=" << fd << ")\n"; }
+	}
+
 	std::string ofname;
 	if(argc > 2){ // User specified output filename.
 		ofname = std::string(argv[2]);
@@ -74,49 +138,112 @@ int main(int argc, char *argv[]){
 
 	if(!output.is_open()){ 
 		std::cout << " ERROR: Failed to open output file '" << ofname << "'!\n";
-		file.close();
+		if(!serial_mode){ file.close(); }
+		else{ serialClose(fd); }
 		return 1; 
 	}
 
 	setup_signal_handlers();
 	
-	output << "time(ms),T(C),P(Torr),R1,R2";
+	output << "time(ms),T(C),P(Torr),R1,R2\n";
 	
+	bool firstRun = true;
 	unsigned int count = 0;
 	while(true){
 		if(SIGNAL_INTERRUPT){
 			break;
 		}
+
+		if(!serial_mode){ // Reading from a binary file. Standard operation.
+			file.read((char*)(&delimiter), 4);
+		
+			if(file.eof()){ break; }
 	
-		file.read((char*)(&delimiter), 4);
-		
-		if(file.eof()){ break; }
-		
-		// Check for the beginning of a new packet.
-		if(delimiter != 0xFFFFFFFF){ continue; }
+			// Check for the beginning of a new packet.
+			if(delimiter != 0xFFFFFFFF){ continue; }
 	
-		// Read data from the file.
-		file.read((char*)(&timestamp), 4);
-		file.read((char*)(&temperature), 4);
-		file.read((char*)(&pressure), 4);
-		file.read((char*)(&relay1), 2);
-		file.read((char*)(&relay2), 2);
+			// Read data from the file.
+			file.read((char*)(&timestamp), 4);
+			file.read((char*)(&temperature), 4);
+			file.read((char*)(&pressure), 4);
+			file.read((char*)(&relay1), 2);
+			file.read((char*)(&relay2), 2);
 		
-		if(file.eof()){ break; }
+			if(file.eof()){ break; }
+		}
+		else{ // Reading from serial.
+			if(firstRun){
+				// Flush whatever is waiting on the port.
+				serialFlush(fd);
+				firstRun = false;
+			}
+
+			int bytesReady = serialDataAvail(fd);
+			if(bytesReady == 0){ // No bytes waiting to be read.
+				// Wait for 10 ms for some bytes to read.
+				usleep(10000);
+				continue;
+			}
+			else if(bytesReady < 0){ // Error on port.
+				std::cout << " ERROR: Encountered error on serial port!\n";
+				break;
+			}			
+
+			if(!ascii_mode){ // Reading binary from serial.
+				if(serialRead(fd, (char*)&delimiter, 4) < 0){
+					std::cout << " ERROR: Encountered error reading on serial port!\n";
+					break;
+				}
 		
-		// Print data to the screen.
-		if(printout){
-			std::cout << " time = " << timestamp/1000 << " s, temp = " << temperature << " C, pres = " << pressure/1000 << " mTorr, R1 = " << relay1 << ", R2 = " << relay2 << "\r";
-			std::cout << std::flush;
+				// Check for the beginning of a new packet.
+				if(delimiter != 0xFFFFFFFF){ continue; }
+		
+				// Read data from the file.
+				if(serialRead(fd, (char*)&timestamp, 4) < 0 ||
+				   serialRead(fd, (char*)&temperature, 4) < 0 ||
+				   serialRead(fd, (char*)&pressure, 4) < 0 ||
+				   serialRead(fd, (char*)&relay1, 2) < 0 ||
+				   serialRead(fd, (char*)&relay2, 2) < 0){
+					std::cout << " ERROR: Encountered error reading on serial port!\n";
+					break;
+				}
+			}
+			else{ // Reading ascii from serial.
+				char *msg = new char[bytesReady+1];
+				if(serialGets(fd, msg, bytesReady+1) < 0){
+					std::cout << " ERROR: Encountered error reading on serial port!\n";
+					break;
+				}
+	
+				// Print data to the screen.
+				if(printout){
+					printf("%s", msg);
+				}
+
+				// Do no processing of the incoming data.
+				// Insert commas and write to the output file.
+				replaceChar(msg, bytesReady+1, '\t', ',');
+				output << msg;
+
+				// Do no further processing.
+				count++;
+				continue;
+			}
 		}
 		
 		// A voltage divider is used in order to get the full
 		// range of 1-8 V from the pressure gauge using the
 		// 5 V arduino. Convert the pressure voltage to the real voltage.
 		pressure = pressure/RPRIME;
-		
+	
 		// Convert the pressure voltage to an actual pressure.
 		pressure = pow(10.0, (pressure-5.0));
+
+		// Print data to the screen.
+		if(printout){
+			std::cout << " time = " << timestamp/1000 << " s, temp = " << temperature << " C, pres = " << pressure*1000 << " mTorr, R1 = " << relay1 << ", R2 = " << relay2 << "\r";
+			std::cout << std::flush;
+		}
 		
 		// Write ascii data to the output file.
 		output << timestamp << ",";
@@ -124,15 +251,18 @@ int main(int argc, char *argv[]){
 		output << pressure << ",";
 		output << relay1 << ",";
 		output << relay2 << "\n";
-		
+
 		count++;
 	}
 	
 	std::cout << "\n Done! Read " << count << " data entries.\n";
 	std::cout << "  Wrote output file '" << ofname << "'\n";
 	
-	file.close();
+	// Close the input file/port.
+	if(!serial_mode){ file.close(); }
+	else{ serialClose(fd); }
+
 	output.close();
-	
+
 	return 0;
 }
